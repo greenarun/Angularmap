362fb599322b75173429b7b8cea484f157558203
greenarun <greenarun@yahoo.com>
2019-01-28 21:59:34 +0800

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { MouseEvent } from '@agm/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  // google maps zoom level
+  zoom: number = 11;
+  
+  // initial center position for the map
+  lat: number = 1.368633;
+  lng: number = 103.826674;
+
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`)
+  }
+  
+  mapClicked($event: MouseEvent) {
+    this.markers.push({
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: true
+    });
+  }
+  
+  markerDragEnd(m: marker, $event: MouseEvent) {
+    console.log('dragEnd', m, $event);
+  }
+  
+  markers: marker[] = [
+	  {
+		  lat: 1.450760,
+		  lng: 103.810900,
+		  label: '20 Woodlands loop',
+		  draggable: true
+	  },
+	  {
+		  lat: 1.319110,
+		  lng: 103.816973,
+      label: 'Botanic gardens',
+		  draggable: false
+	  }
+  ]
+}
+
+// just an interface for type safety.
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+	draggable: boolean;
+}
